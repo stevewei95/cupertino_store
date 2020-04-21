9736b0b20e8ec1b89a67452fcbd7006816f4a06c
weixing13 <weixing@jd.com>
2020-04-21 20:40:39 +0800

    initial app, add structure for a 3-tab, add state management

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'app.dart';
+import 'model/app_state_model.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // This app is designed only to work vertically, so we limit
+  // orientations to portrait up and down.
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (context) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreApp(),
+    ),
+  );
+}
